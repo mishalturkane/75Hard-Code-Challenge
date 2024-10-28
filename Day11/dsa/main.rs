0413469ced90315c36fdcc357d71904a40c478f1
user.name: mishalturkane <mishalturkane@gmail.com>
2024-10-28 20:04:32 +0530

    Day11 Added to the 75-Hard-Code-Challenge

--- Day11/dsa/main.rs
+++ Day11/dsa/main.rs
@@ -1,7 +1,16 @@
 fn main() {
-    let arr = [1, 2, 3, 4, 5];
-    arr.push(10);
-    for i in arr {
-        println!("{}", i);
+    let name = String::from("mishal");
+    let bytes = name.as_bytes(); // Convert the string to a byte array
+    let mut reversed_name = String::new(); // Create an empty String to store the reversed name
+
+    // Iterate through the byte array from the last index to the first
+    let mut i = bytes.len();
+    while i > 0 {
+        i -= 1;
+        // Push each character from the end of the array to the new string
+        reversed_name.push(bytes[i] as char);
     }
+
+    println!("Original: {}", name);
+    println!("Reversed: {}", reversed_name);
 }
